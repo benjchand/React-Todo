c42ee47521aa38f7dea9a375fde42a39a75ff7f2
benjchand <benchand@gmail.com>
2020-09-03 07:14:23 -0700

    Who watches the watchers?

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  // I am changing this to see what happens.
+
   constructor() {
     super()
     this.state = {
