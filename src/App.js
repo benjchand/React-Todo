699f5f07831893ece85a476a419a0b03af97e46e
benjchand <benchand@gmail.com>
2020-09-03 04:13:23 -0700

    Reworking code to implement new React skills.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      toDoList: [{
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: true
+      },
+      {
+        task: 'Empty Garage',
+        id: 1528817077287,
+        completed: false
+      }],
+      newTask: '',
+      hideCompleted: true
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
